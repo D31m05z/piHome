31b5af619600ad65b3a4eaf49e8ac825f96ab19a
gergely.boross <sk8geri@gmail.com>
2019-02-28 15:56:54 +0100

    refat build.sh
    Signed-off-by: gergely.boross <sk8geri@gmail.com>

--- 3rdparty/build.sh
+++ 3rdparty/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -ex
 
-cmake -DCMAKE_INSTALL_PREFIX=install -Hglm -Bbuild/glm
-cmake --build build/glm --target install
+function build {
+    echo "----- BUILD AND INSTALL = <$1> -----"
 
-cmake -DCMAKE_INSTALL_PREFIX=install -DGLFW_USE_EGL=true -DGLFW_CLIENT_LIBRARY="glesv2" -Hglfw -Bbuild/glfw
-cmake --build build/glfw --target install
+    cmake -DCMAKE_INSTALL_PREFIX=install -H$1 -Bbuild/$1
+    cmake --build build/$1 --target install
+}
 
-cmake -DCMAKE_INSTALL_PREFIX=install -DGLFW_USE_EGL=true -DGLFW_CLIENT_LIBRARY="glesv2" -Himgui -Bbuild/imgui
-cmake --build build/imgui --target install
+build glm
+build glfw
+build imgui
+build raspicam
 
-cmake -DCMAKE_INSTALL_PREFIX=install -Hraspicam -Bbuild/raspicam
-cmake --build build/raspicam --target install
