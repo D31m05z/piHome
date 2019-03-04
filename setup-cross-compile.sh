901382b9caebbaec20c2dcc13d0bd26cb131b028
gergely.boross <sk8geri@gmail.com>
2019-03-04 18:01:34 +0100

    sync /lib and /usr folder from rasberry device for cross compile
    Signed-off-by: gergely.boross <sk8geri@gmail.com>

--- setup-cross-compile.sh
+++ setup-cross-compile.sh
@@ -4,6 +4,9 @@
 # apt-get install git rsync cmake ia32-libs
 # the ia32-libs is not available on Debian, you can use lib32z1 instead
 
+read -s -p "Pi username: " username
+read -s -p "Pi address: " address
+
 # create rasberrypi folder to cross compile packages
 [ -d raspberrypi ] || mkdir raspberrypi
 pushd raspberrypi
@@ -11,11 +14,13 @@ pushd raspberrypi
 # clone rasberrypi/tools git repo from github
 git clone git://github.com/raspberrypi/tools.git
 
-#export PATH=$PATH:$HOME/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin
-
 # create rootfs folder and copy the entire /lib and /usr directory from rasberrypi
-
-#mkdir rootfs
-#rsync -rl --delete-after --safe-links pi@192.168.0.52:/{lib,usr} $PWD/raspberrypi/rootfs
+if [ ! -d rootfs ];
+then
+    mkdir rootfs
+    rsync -rl --delete-after --safe-links $username@$address:/{lib,usr} $PWD/raspberrypi/rootfs
+fi
 
 popd    # raspberrypi
+
+export PATH=$PATH:$PWD/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin
