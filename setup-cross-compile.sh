868bcf230389747f1ae76774551929af2dcfada3
gergely.boross <sk8geri@gmail.com>
2019-03-04 18:12:03 +0100

    fix: clone tools already exists
    We can source this setup-cross-compile.sh script and we can use the toolchain file.
    
    	Ex.: $ source setup-cross-compile.sh
                 $ cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE -H. -Bbuild
    	     $ cmake --build build
    
    Signed-off-by: gergely.boross <sk8geri@gmail.com>

--- setup-cross-compile.sh
+++ setup-cross-compile.sh
@@ -4,23 +4,29 @@
 # apt-get install git rsync cmake ia32-libs
 # the ia32-libs is not available on Debian, you can use lib32z1 instead
 
-read -s -p "Pi username: " username
-read -s -p "Pi address: " address
-
 # create rasberrypi folder to cross compile packages
-[ -d raspberrypi ] || mkdir raspberrypi
-pushd raspberrypi
+if [ -d raspberrypi ];
+then
+    mkdir raspberrypi
 
-# clone rasberrypi/tools git repo from github
-git clone git://github.com/raspberrypi/tools.git
+    pushd raspberrypi
+
+    # clone rasberrypi/tools git repo from github
+    git clone git://github.com/raspberrypi/tools.git
+    popd
+fi
 
 # create rootfs folder and copy the entire /lib and /usr directory from rasberrypi
 if [ ! -d rootfs ];
 then
+    read -s -p "Pi username: " username
+    read -s -p "Pi address: " address
+
+    pushd raspberrypi
     mkdir rootfs
     rsync -rl --delete-after --safe-links $username@$address:/{lib,usr} $PWD/raspberrypi/rootfs
+    popd
 fi
 
-popd    # raspberrypi
-
 export PATH=$PATH:$PWD/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin
+export TOOLCHAIN_FILE=$PWD/toolchain/pi.cmake
