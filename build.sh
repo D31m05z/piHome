135936646ff755d0b4bc519ae24b225035954655
gergely.boross <sk8geri@gmail.com>
2019-03-04 17:17:25 +0100

    build refactor
    - Use one build script for building dependencies and main project.
    - Parallel build support (-j8).
    - Toolchain file support (for cross compile).
    
    Signed-off-by: gergely.boross <sk8geri@gmail.com>

--- build.sh
+++ build.sh
@@ -1,17 +1,27 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 export CMAKE_PREFIX_PATH=$PWD/3rdparty/install
 
 if [[ "$USE_CROSS_COMPILE" == true ]];
 then
     export PATH=$PATH:$HOME/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin
-
-    # configure
-    cmake -DCMAKE_INSTALL_PREFIX=install -DCMAKE_TOOLCHAIN_FILE=$HOME/raspberrypi/pi.cmake -H. -Bbuild
-else
-    # configure
-    cmake -DCMAKE_INSTALL_PREFIX=install -H. -Bbuild
+    export TOOLCHAIN_FILE=$HOME/raspberrypi/pi.cmake
 fi
 
-# build
-cmake --build build
+function build {
+    echo "----- BUILD AND INSTALL = <$1> -----"
+    PACKAGE=$(basename $1)
+
+    cmake -DCMAKE_INSTALL_PREFIX=install -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE -H$1 -Bbuild/$PACKAGE
+    cmake --build build/$PACKAGE --target install --parallel 8
+}
+
+# build dependencies
+build 3rdparty/glm
+build 3rdparty/glfw
+build 3rdparty/imgui
+build 3rdparty/raspicam
+#build 3rdparty/wiringpi
+
+# build main project
+build piHome
