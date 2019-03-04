59725c980c22c32bc6d546f57c114ce49b91ea52
gergely.boross <sk8geri@gmail.com>
2019-03-04 18:02:30 +0100

    PARALLEL and CMAKE_PREFIX_PATH build parameter
    Check rootfs folder exists for cross compile.
    
    Signed-off-by: gergely.boross <sk8geri@gmail.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -ex
 
-export CMAKE_PREFIX_PATH=$PWD/3rdparty/install
+: ${CMAKE_PREFIX_PATH:="$PWD/3rdparty/install"}
+: ${PARALLEL:="8"}
 
 if [[ "$USE_CROSS_COMPILE" == true ]];
 then
     [ ! -d raspberrypi ] && echo "Cross compile environment is not prepared. Run the setup-cross-compile.sh script first." && exit 1
+    [ ! -d raspberrypi/rootfs ] && echo "Rootfs folder is not exists. \
+                                         Run the setup-cross-compile.sh script again \
+                                         and fill the correct IP address and username." && exit 1
 
     export PATH=$PATH:$PWD/raspberrypi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin
     export TOOLCHAIN_FILE=$PWD/toolchain/pi.cmake
@@ -15,7 +19,7 @@ function build {
     PACKAGE=$(basename $1)
 
     cmake -DCMAKE_INSTALL_PREFIX=install -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE -H$1 -Bbuild/$PACKAGE
-    cmake --build build/$PACKAGE --target install --parallel 8
+    cmake --build build/$PACKAGE --target install --parallel $PARALLEL
 }
 
 # build dependencies
