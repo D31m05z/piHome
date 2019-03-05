306a1bf185519eaf6fa10f10ec0caeac92817598
Syrt1s M4j0r <d31m05z@gmail.com>
2019-03-05 14:40:51 -0500

    build fix on rasberry : use glesv2 GLFW_CLIENT
    Signed-off-by: Syrt1s M4j0r <d31m05z@gmail.com>

--- build.sh
+++ build.sh
@@ -18,13 +18,14 @@ function build {
     echo "----- BUILD AND INSTALL = <$1> -----"
     PACKAGE=$(basename $1)
 
-    cmake -DCMAKE_INSTALL_PREFIX=install -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE -H$1 -Bbuild/$PACKAGE
+    cmake -DCMAKE_INSTALL_PREFIX=install -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE -H$1 -Bbuild/$PACKAGE $2
     cmake --build build/$PACKAGE --target install --parallel $PARALLEL
 }
 
 # build dependencies
+#build 3rdparty/glshim
 build 3rdparty/glm
-build 3rdparty/glfw
+build 3rdparty/glfw "-DGLFW_USE_EGL=true -DGLFW_CLIENT_LIBRARY=glesv2"
 build 3rdparty/imgui
 build 3rdparty/raspicam
 #build 3rdparty/wiringpi
